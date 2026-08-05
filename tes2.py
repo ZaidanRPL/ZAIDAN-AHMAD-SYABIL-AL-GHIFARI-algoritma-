154e41f2fa3de718415800d0b27c95618826515f
Zaidan <rplzaidan@gmail.com>
2026-08-05 08:20:49 +0700

    Perhitungan Phyton

---
+++ tes2.py
@@ -0,0 +1,72 @@
+print("tes lagii")
+
+a = 4
+b = 7
+
+hasil = a * b
+print("hasil:",hasil)
+
+print("tes lagii")
+
+import time
+time.sleep(1)
+
+a = 2
+b = 8
+
+hasil = a + b
+print("hasil:",hasil)
+
+import time
+time.sleep(1)
+
+a = 2
+b = 8
+
+hasil = a ** b 
+print("hasil:",hasil)
+
+import time
+time.sleep(2)
+
+a = 9
+b = 4
+
+hasil = a % b
+print("hasil:",hasil)
+
+import time
+time.sleep(1)
+
+a = 2
+b = 8
+
+hasil = a / b
+print("hasil:",hasil)
+
+import time
+time.sleep(1)
+
+a = 2
+b = 8
+
+hasil = a + b
+print("hasil:",hasil)
+
+import time
+time.sleep(1)
+
+a = 2
+b = 8
+
+hasil = a - b
+print("hasil:",hasil)
+
+import time
+print("tes...")
+time.sleep(1) 
+print("he he~~")
+time.sleep(3)
+print("saya akan lawan")
+
+range(5,0)
