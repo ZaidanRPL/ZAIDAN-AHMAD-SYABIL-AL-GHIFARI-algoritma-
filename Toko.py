9a3cf2561c8ff469a0ca9fda85d470ad3fdeca86
Zaidan <rplzaidan@gmail.com>
2026-08-05 12:45:13 +0700

    Penjualan Toko Mainan

---
+++ Toko.py
@@ -0,0 +1,77 @@
+Produk="Rubik 2x2"
+Stok=15
+Harga=5.5
+Tersedia=True
+
+print("======================================================")
+print("Toko Mainan Asli Jupiter")
+print("======================================================") 
+
+import time
+time.sleep(1)
+print("======================================================")
+print("Produk  :", Produk)
+print("Stok    :", Stok) 
+print("Harga $ :", Harga) 
+print("Tersedia:", Tersedia)
+print("======================================================")
+
+A = Stok * Harga
+
+print("Total Hasil")
+print(Stok * Harga)
+import time
+time.sleep(1)
+Produk="Rubik 3x3"
+Stok=15
+Harga=7.5
+Tersedia=True
+
+print("======================================================")
+print("Produk  :", Produk)
+print("Stok    :", Stok) 
+print("Harga $ :", Harga) 
+print("Tersedia:", Tersedia)
+print("======================================================")
+B = Stok * Harga
+print(Stok * Harga)
+print("Total Hasil")
+Produk="Rubik 4x4"
+Stok=15
+Harga=11.5
+Tersedia=True
+import time
+time.sleep(1)
+print("======================================================")
+print("Produk  :", Produk)
+print("Stok    :", Stok) 
+print("Harga $ :", Harga) 
+print("Tersedia:", Tersedia)
+print("======================================================")
+
+C = Stok * Harga
+print(Stok * Harga)
+print("Total Hasil")
+
+Produk="Rubik 5x5"
+Stok=15
+Harga=14.5
+Tersedia=True
+import time
+time.sleep(1)
+print("======================================================")
+print("Produk  :", Produk)
+print("Stok    :", Stok) 
+print("Harga $ :", Harga) 
+print("Tersedia:", Tersedia)
+print("======================================================")
+
+print(Stok * Harga)
+print("חנות הצעצועים האותנטית ג'ופיטר")
+
+D = Stok * Harga
+print(Stok * Harga)
+print("Total Hasil")
+
+print("Total hasil penjualan")
+print(A + B + C + D)
