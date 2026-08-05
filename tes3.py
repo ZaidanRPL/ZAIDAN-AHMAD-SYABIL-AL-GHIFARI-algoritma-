05b72a39d38237749c170279ebc5a9f72144a034
Zaidan <rplzaidan@gmail.com>
2026-08-05 08:18:51 +0700

    perkenalan di Phtyon

---
+++ tes3.py
@@ -0,0 +1,15 @@
+#tugas saya
+import time
+print("----------------------------------")
+print("Selamat datang entah siapapun anda")
+print("ꦯꦺꦭꦩꦠ꧀ ꦢꦠꦤ꧀ꦒ꧀ ꦌꦤ꧀ꦠꦃ ꦱꦶꦪꦥꦥꦸꦤ")
+print("----------------------------------")
+print()
+print("Saya...iya saya Zaidan Ahmad S.A kelas 10 RPL 2")
+print("Sekolah:saya adalah seorang murid SMK TJP Tuban")
+print("Tahun:2026")
+
+time.sleep(1)
+print("Tes")
+time.sleep(1)
+print("tesss")
