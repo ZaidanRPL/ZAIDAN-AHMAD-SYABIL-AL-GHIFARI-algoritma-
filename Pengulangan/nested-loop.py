1ceec7af6fec89119b226474b833097db9cec91a
Zaidan <rplzaidan@gmail.com>
2026-09-09 09:35:16 +0700

    Add files via upload

---
+++ Pengulangan/nested-loop.py
@@ -0,0 +1,14 @@
+n = int(input("Masukan Angka:"))
+i = 1
+while i <= n :
+      if i % 2 == 0:
+            print("Bilangan genap")
+      else:
+            print(i)
+      i += 1
+
+
+for i in range(1,4):
+    for j in range(1,4):
+            print(i, j,)
+    
